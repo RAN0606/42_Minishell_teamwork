aec5f838617cf354943fdffc2b85b0a9e6a6ddf2
Qin XIA <qxia@e1r11p3.42.fr>
2022-06-10 15:35:39 +0200

    exit

--- builtins/ft_exit.c
+++ builtins/ft_exit.c
@@ -6,15 +6,40 @@
 /*   By: qxia <qxia@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 12:05:16 by qxia              #+#    #+#             */
-/*   Updated: 2022/06/03 14:47:19 by qxia             ###   ########.fr       */
+/*   Updated: 2022/06/09 14:58:25 by qxia             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void    ft_error(char **args,)
+static int  ft_strisdigit(char *str)
 {
+    size_t  i;
+
+    if (!str)
+        return (0);
+    i = 0;
+    while (str[i])
+    {
+        if (ft_isdigit(str[i]) == 0)
+            return (0);
+        i++;
+    }
+    return (1);
+}
+
+static int  check_nbr(char *str)
+{
+    int i;
     
+    i = 0;
+    while (str[i])
+    {
+        if (str[i] == '-' || str[i] == '+')
+            return (0);
+        i++;
+    }
+    return (1);
 }
 
 void    ft_exit(char **inputs, t_data *data)
